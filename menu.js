e31cc1a4739806a532c5d147b8a73b9aad790aa3
hhemkraivuth <hemkraivuth.h@gmail.com>
2026-09-10 19:42:18 +0700

    Update menu.js

--- menu.js
+++ menu.js
@@ -74,7 +74,7 @@ const MENU = [
   { id: "drink_enziero_golden_apple_yuzu", name: "EN Ziero - Golden Apple & Yuzu", price: 38, category: "drinks", image: "https://res.cloudinary.com/x2znhtgb/image/upload/v1789041110/ENZiero_GoldenAppleandYuzu.heic" },
   { id: "drink_juza_sparkling", name: "Juza - Sparkling Orange, Lemon", price: 48, category: "drinks", image: "https://res.cloudinary.com/x2znhtgb/image/upload/v1789041103/Juza_SparklingOrangeLemon.heic" },
   { id: "drink_coke_original", name: "Coke Original", price: 38, category: "drinks", image: null },
-  { id: "drink_coke_zero", name: "Coke Zero Zero", price: 38, category: "drinks", image: "https://res.cloudinary.com/x2znhtgb/image/upload/v1789041103/Juza_SparklingOrangeLemon.heic" },
+  { id: "drink_coke_zero", name: "Coke Zero Zero", price: 38, category: "drinks", image: "https://res.cloudinary.com/x2znhtgb/image/upload/v1789043475/Coke_ZeroZero.heic" },
   { id: "drink_eden_green_apple", name: "Eden - Green Apple Sparkling", price: 38, category: "drinks", image: "https://res.cloudinary.com/x2znhtgb/image/upload/v1789043475/EdenGreen_AppleSparkling.heic" },
   { id: "drink_prebo_classic_muscat", name: "Prebo Pop - Classic Muscat", price: 48, category: "drinks", image: "https://res.cloudinary.com/x2znhtgb/image/upload/v1789043474/PreboPop_ClassicMuscat.heic" },
   { id: "drink_prebo_cream_cloud", name: "Prebo Pop - Cream Cloud", price: 48, category: "drinks", image: "https://res.cloudinary.com/x2znhtgb/image/upload/v1789041102/prebo_creamcloud.jpg" },
